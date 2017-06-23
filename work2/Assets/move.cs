a735835119b159f8850c3e02f287da36c7b1758c
An0910 <dolce54765@gmail.com>
2017-06-23 10:58:23 +0800

    Summyx

---
+++ work2/Assets/move.cs
@@ -0,0 +1,18 @@
+﻿public var moveSpeed : float = 10f;
+public var turnSpeed : float = 50f;
+ 
+ 
+function Update()
+{
+    if (Input.GetKey(KeyCode.UpArrow))
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.DownArrow))
+        transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.LeftArrow))
+        transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+    if (Input.GetKey(KeyCode.RightArrow))
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+}
